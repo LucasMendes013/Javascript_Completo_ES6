cddb4476f4ac4f71511b1caa532484dfe8fda645
LucasMendes013 <104436533+LucasMendes013@users.noreply.github.com>
2022-06-08 12:17:48 -0300

    Funções aula 2
    um pouco mais do return
    hoisting em funções
    funções dentro de funções(escopo léxico)
    como funciona o escopo dentro de funções

--- Functions/function.js
+++ Functions/function.js
@@ -91,4 +91,64 @@ function exemplo(){
   addEventListener('click', exemplo)
 
 
+//Pode ou não retornar um valor, mas quando ela retorna, ela só retorna um valor
+//Quando não definimos um return, ela irá retornar undefined. O código interno da função é executado normalmente, independente de existir valor no return ou não.
+function imc(peso, altura){
+  const imc = peso / (altura ** 2)
+  console.log(imc)
+}
+
+imc(80, 1.80)             //retorna o imc
+console.log(imc(80,1.80)) //retorna o imc e undefined
+
+
+//Valores retornados
+//Uma função pode retornar qualquer tipo de dados, e até outras funções
+//Cuidado, retornar vários tipos de dados em uma mesma função não é uma boa ideia
+function terceiraIdade(idade){
+    if(typeof idade !== 'number') {       //console.log(terceiraIdade('aaaa'))
+      return 'Informe a sua idade!'
+  } else if(idade >= 60){                 //console.log(terceiraIdade(60))
+    return true
+  } else {                                //console.log(terceiraIdade(20))
+    return false
+  }
+}
+
+
+//ESCOPO
+//Variáveis e funções definidas dentro de um bloco {}, não são visíveis fora dele
+//Podemos também colocar afrente do retun um template string
+function precisoVisitar (paisesVisitados) {
+  var totalPaises = 193
+  return `Ainda faltam ${totalPaises - paisesVisitados} países`
+}
+
+precisoVisitar(100)
+
+//ESCOPO LÉXICO
+//Funções conseguem acessar variáveis que foram criadas no contexto pai
+var profissao = 'Designer'
+
+function dados() {
+    var nome = 'André'
+    var idade = 28
+  function outrosDados(){
+    var endereco = 'Rio de Janeiro'
+    var idade = 29
+    return `${nome},${idade},${endereco},${profissao}`
+  }
+  return outrosDados()
+}
+
+console.log(dados())
+
+//HOISTING
+//Antes de executar uma função, o JS 'move' todas as funções declaradas para a memória
+imc(80, 1.80)
+
+function imc(peso, altura) {
+  const imc = peso / (altura ** 2)
+  console.log(imc)
+}
 
