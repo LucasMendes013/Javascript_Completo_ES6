ea6f4d2c468deebed750548726757cdb701f3c53
LucasMendes013 <104436533+LucasMendes013@users.noreply.github.com>
2022-06-09 11:20:13 -0300

    Commit Arrays e Loops1

--- ArrayseLoops/array.js
+++ ArrayseLoops/array.js
@@ -8,7 +8,7 @@ videoGame[0]
 videoGame[2]
 
 //Métodos e propriedades de um array
-var videoGame = ['Switch', 'PS4', 'Xbox']
+var videoGame = ['Switch', 'PS4', 'Xbox', '3DS']
 videoGame.pop()       //Remove o último item e retorna ele
 videoGame.push('3ds') //Adiciona ao final da array
 videoGame.length      //3
@@ -37,4 +37,34 @@ var i = 0
 while (i < 10){
   console.log(i)
   i++
+}
+
+//Exemplo
+//Break: para a condição
+var videoGame = ['Switch', 'PS4', 'Xbox', '3DS']
+for(var item = 0; item < videoGame.length; item++){
+  console.log(videoGame[item])
+  if(videoGame[i] === 'PS4'){
+    break;
+  }
+}
+
+//forEach
+//forEach é um método de Array que executa uma função para cada item da Array. É uma forma mais simples de utilizarmos um loop com arrays(ou array-like)
+//Podemos passar o seguintes valores: item, index, array
+var videoGame = ['Switch', 'PS4', 'Xbox', '3DS']
+videoGame.forEach(function(item,index){
+  console.log(item,index)
+})
+//O argumento item será atribuido dinamicamente
+
+//Não se confunda com a sintaxe
+var frutasComidas = 0
+var frutas = 50
+
+//Podemos até mesmo colocar apenas a condição dentro do for, que é o mais importante
+
+for(;frutasComidas <= frutas;) {
+  console.log(frutasComidas)
+  frutasComidas = frutasComidas + 2
 }
